21f134d729ba3cb062313628ca87cc51e4387a23
Walter <sebastian.taka@hotmail.com>
2019-04-15 19:43:15 -0300

    Agregados los tests para el auto

--- src/main/java/vehiculos/Auto.java
+++ src/main/java/vehiculos/Auto.java
@@ -16,13 +16,55 @@ public class Auto implements Vehiculo{
 	 @Injected(count=4)
 	 private Rueda[] ruedas;
 
-	 @Injected(count=4,implementation=List.class)
+	 @Injected(count=5,implementation=List.class)
 	 private List<Asiento> asientos;
 	 
-	 public String queseyo = "jijr";
-
+	 private String queseyo = "jijr";
+	 
+	 private Boolean tieneQueSerNulo;
+	 
 	 public void printProperties() {
-		 System.out.println(motor);
-		 motor.printProperties();
+		 System.out.println(getMotor());
+		 getMotor().printProperties();
 	 }
+
+	public Motor getMotor() {
+		return motor;
+	}
+
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+	public Rueda[] getRuedas() {
+		return ruedas;
+	}
+
+	public void setRuedas(Rueda[] ruedas) {
+		this.ruedas = ruedas;
+	}
+
+	public Boolean getTieneQueSerNulo() {
+		return tieneQueSerNulo;
+	}
+
+	public void setTieneQueSerNulo(Boolean tieneQueSerNulo) {
+		this.tieneQueSerNulo = tieneQueSerNulo;
+	}
+
+	public String getQueseyo() {
+		return queseyo;
+	}
+
+	public void setQueseyo(String queseyo) {
+		this.queseyo = queseyo;
+	}
+
+	public List<Asiento> getAsientos() {
+		return asientos;
+	}
+
+	public void setAsientos(List<Asiento> asientos) {
+		this.asientos = asientos;
+	}
 }
---
+++ src/test/java/dependencyInjection/FactoryAutoTest.java
@@ -0,0 +1,64 @@
+package dependencyInjection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import vehiculos.Auto;
+import dependencyInjection.Factory;
+
+public class FactoryAutoTest {
+	
+	Auto auto;
+	
+	@Before
+	public void init() {
+		auto = Factory.getObject(Auto.class);
+	}
+	
+	@Test
+	public void inyectorNoDevuelveNull() {
+		assert( auto != null);
+	}
+	
+	@Test
+	public void autoTieneMotor() {
+		assert( auto.getMotor() != null);
+	}
+	
+	@Test
+	public void autoTieneRuedas() {
+		assert( auto.getRuedas() != null);
+	}
+	
+	@Test
+	public void autoNoTieneAlgoQueNoInstancio() {
+		assert( auto.getTieneQueSerNulo() == null);
+	}
+	
+	@Test
+	public void autoTieneAsientos() {
+		assert( auto.getAsientos() != null);
+	}
+	
+	@Test
+	public void autoTienePropiedadInstanciadaPorClase() {
+		assert( auto.getQueseyo() != null);
+	}
+	/* ****TODO****
+	@Test
+	public void motorEsSingleton() {
+		Auto auto2 = Factory.getObject(Auto.class);
+		assert ( auto == auto2 );
+	}
+	*/
+	
+	@Test
+	public void autoTieneArrayDeCuatroRuedas() {
+		assert( auto.getRuedas().length == 4);
+	}
+	
+	@Test
+	public void autoTieneListaDe5Asientos() {
+		assert( auto.getAsientos().size() == 5);
+	}
+}
